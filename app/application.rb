38bf6270a2e066c76712be190876354a4b7ef316
Annie Bourke <aabourke@gmail.com>
2018-05-14 22:46:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    current_time = time.now
+    current_time = Time.now.hour
 
     if current_time < 12
       resp.write "Good Morning!"
