f10d6798fd814a565050dd0d758198c46ede8483
Annie Bourke <aabourke@gmail.com>
2018-05-14 22:43:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,13 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+
+    current_time = time.now
+
+    if current_time < 12
+      resp.write "Good Morning!"
     else
-      resp.write "You Lose"
+      resp.write "Good Afternoon!"
     end
 
     resp.finish
